faf2feac6bd50de3fd138e3f764f4d5b8ac14d26
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-01-05 16:51:51 +0300

    add location description

--- src/components/Prison-page/PrisonLocation.js
+++ src/components/Prison-page/PrisonLocation.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Map from '../Map/Map';
 import TextInput from './TextInput';
+import Map from '../Map/Map';
 
 const PrisonLocation = (props) => {
-  const { prison } = props;
+  const {prison} = props;
   const features = prison.features || [];
   return (
     <div className="prison__location">
@@ -13,20 +13,22 @@ const PrisonLocation = (props) => {
             let className = features[0] === location ?
               'field-title__location field-title__location_active' : 'field-title__location';
 
-            return <div className={ className }
-                        key={ key }
-            >
-              Локация { key + 1 }
-            </div>
+            return (
+              <div className={ className }
+                   key={ key }
+              >
+                Локация { key + 1 }
+              </div>
+            )
           })
         }
         <button className="field-title__plus">+</button>
       </div>
-      <TextInput />
+      <TextInput defaultValue={ prison.features[0].properties.location }/>
       <TextInput />
       <Map features={ prison.features }/>
     </div>
   );
-}
+};
 
 export default PrisonLocation;
