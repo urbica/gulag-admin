a4db4f4519369b9e351ed0c82e7fa4dfd7f5cd34
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-01-10 15:09:47 +0300

    change SelectInput styles

--- src/components/Inputs/SelectInput.js
+++ src/components/Inputs/SelectInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+import './SelectInput.css';
 
 const SelectInput = (props) => {
   return (
     <Select {...props} />
   );
-}
-
+};
 
 export default SelectInput;
