03dcc11a9e93c467b32af5f1c05fce6fe72af1f8
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-01-11 15:22:43 +0300

    fix styles

--- src/components/PrisonsTable/PrisonRow.js
+++ src/components/PrisonsTable/PrisonRow.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import {browserHistory} from 'react-router';
 
 const PrisonRow = (props) => {
-  const { prison } = props;
+  const {prison} = props;
   const features = prison.features || [];
   const url = `/admin/prisons/${prison.id}`;
   const openPrison = browserHistory.push.bind(browserHistory, url);
@@ -24,12 +24,12 @@ const PrisonRow = (props) => {
       <td className='prisons__cell'>время</td>
       <td className='prisons__cell'>регион</td>
       <td className='prisons__cell prisons__strength'>
-        { String(prison.max_prisoners).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ') }
+        <span>{ String(prison.max_prisoners).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ') }</span>
       </td>
       <td className='prisons__cell'>{ prison.published_ru ? 'да' : 'нет' }</td>
       <td className='prisons__cell'>{ prison.published_en ? 'да' : 'нет' }</td>
     </tr>
   );
-}
+};
 
 export default PrisonRow;
