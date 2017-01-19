b2bc48fa19d8f159b7e55c4b6df44eba82027d51
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-01-19 17:54:09 +0300

    fix updated_at display

--- src/components/IndexPage/PrisonsTable/PrisonRow.js
+++ src/components/IndexPage/PrisonsTable/PrisonRow.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {browserHistory} from 'react-router';
 
+const getPeriods = (features) =>
+  features
+    .map(feature => Object.keys(feature.properties).map(year => parseInt(year, 10)))
+    .filter(years => years.length > 0)
+    .map(years => `${Math.min.apply(Math, years)} — ${Math.max.apply(Math, years)}`)
+    .join('; ');
+
 const PrisonRow = (props) => {
   const {prison} = props;
   const features = prison.features || [];
@@ -10,18 +17,9 @@ const PrisonRow = (props) => {
     <tr onClick={ openPrison }>
       <td className='prisons__cell' height='56'>{prison.name_ru}</td>
       <td className='prisons__cell prisons__cell_period'>
-        {
-          features.map((location, key) => {
-            const YEARS = Object.keys(location.properties);
-            if (YEARS.length === 1) {
-              return <div key={ key }>{ YEARS[0] };</div>
-            } else if (YEARS.length > 1) {
-              return <div key={ key }>{ YEARS[0] + ' – ' + YEARS[YEARS.length - 1] };</div>
-            } else return null
-          })
-        }
+        { getPeriods(features) }
       </td>
-      <td className='prisons__cell'>{ prison.updated_at }</td>
+      <td className='prisons__cell'>{ (new Date(prison.updated_at)).toLocaleString() }</td>
       <td className='prisons__cell'>регион</td>
       <td className='prisons__cell prisons__strength'>
         <span>{ String(prison.max_prisoners).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ') }</span>
