0ee67301da3f709e9725ce4de5551e371ad1f9aa
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-01-24 17:48:20 +0300

    login page

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoginPageWrap = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
+
+const LoginForm = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 500px;
+  margin: auto;
+`;
+
+const InputWrap = styled.label`
+  position: relative;
+  width: 100%;
+  margin-bottom: 5px;
+  & input {
+    width: 100%;
+    padding: 0;
+    border: none;
+    border-bottom: 1px solid rgba(0,0,0,.2);
+    font-family: 'PT Sans';
+    font-size: 48px;
+    font-weight: bold;
+    outline: none;
+  }
+`;
+
+const Enter = styled.button`
+  position: absolute;
+  right: 0;
+  padding: 23px 5px 4px 54px;
+  border-radius: 4px;
+  background-color: #fff;
+  border: solid 1px rgba(0, 0, 0, 0.5);
+  font-family: 'PT Sans';
+  font-size: 15px;
+  outline: none;
+  opacity: 0;
+  z-index: -10;
+  transition: opacity .3s;
+  input:valid + & {
+    z-index: inherit;
+    opacity: 1;
+    transition: opacity .3s;
+  }
+  &:hover {
+    background-color: #f0f0f0;
+  }
+  &:active {
+    border-color: #000;
+  }
+`;
+
+const PasswordLine = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 0;
+  height: 2px;
+  background-color: #000;
+  transition: width .3s;
+  input:focus ~ & {
+    width: 100%;
+    transition: width .3s;
+`;
+
+const PasswordReminder = styled.a`
+  margin-left: auto;
+  color: rgba(0,0,0,.5);
+  text-decoration: none;
+`;
+
+class LoginPage extends React.PureComponent {
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      const password = this.refs.password;
+      this.props.onSubmit(password.value);
+    }
+  };
+
+  onClick = () => {
+    const password = this.refs.password;
+    this.props.onSubmit(password.value);
+  };
+
+  componentDidMount() {
+    this.refs.password.focus();
+  }
+
+  render() {
+    return (
+      <LoginPageWrap>
+        <LoginForm>
+          <div>Пароль</div>
+          <InputWrap>
+            <input
+              required
+              ref='password'
+              type='password'
+              placeholder='Введите пароль'
+              onKeyPress={ this.handleKeyPress }
+            />
+            <Enter onClick={ this.onClick }>Enter</Enter>
+            <PasswordLine/>
+          </InputWrap>
+          <PasswordReminder href='#'>Напомнить</PasswordReminder>
+        </LoginForm>
+      </LoginPageWrap>
+    );
+  }
+}
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './components/App/App';
-import LoginPage from './components/LoginPage/LoginPage';
+import LoginPage from './components/LoginPage/LoginPage.jsx';
 import IndexPage from './components/IndexPage/IndexPage.jsx';
 import PrisonPage from './components/PrisonPage/PrisonPage.jsx';
-import {Router, Route, IndexRedirect, IndexRoute, browserHistory} from 'react-router'
+import { Router, Route, IndexRedirect, IndexRoute, browserHistory } from 'react-router'
 
 // if (module.hot) {
 //   module.hot.accept()
