9c04d7728ad05e7706b5e8fc68f3e83a1005a28a
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-01-24 21:30:26 +0300

    fix lat long input

--- src/components/PrisonPage/PrisonLocation/CoordinatesInput.js
+++ src/components/PrisonPage/PrisonLocation/CoordinatesInput.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import TextInput from '../Inputs/TextInput';
+import styled from 'styled-components';
+
+const InputWrap = styled.label`
+  position: relative;
+  padding-left: 120px;
+  padding: 8px 8px 5px;
+  margin-left: auto;
+  background-color: #f3f3f3;
+  &:hover {
+    background-color: #f9f9f9;
+  }
+`;
+
+const InputDesc = styled.span`
+  opacity: .5;
+`;
+
+const Input = styled.input`
+  width: auto !important;
+  border: none;
+  border-radius: 0;
+  font-family: 'PT Sans', sans-serif;
+  background-color: #f3f3f3;
+  outline: none;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: right;
+`;
 
 const CoordinatesInput = (props) => {
-  const {coordinates, updateCoordinates} = props;
-  const value = coordinates[0] + ', ' + coordinates[1];
+  const { coordinates, updateCoordinates } = props;
+  const value = `${coordinates[0]}, ${coordinates[1]}`;
 
   const changeHandler = (event) => {
-    const {value} = event.target;
+    const { value } = event.target;
     const newCoordinates = value.split(/, /);
     updateCoordinates(newCoordinates);
   };
 
   return (
-    <TextInput
-      value={ value }
-      onChange={ changeHandler }
-    />
+    <InputWrap>
+      <InputDesc>долгота, широта</InputDesc>
+      <Input
+        className='input'
+        type='text'
+        value={ value }
+        onChange={ changeHandler }
+      />
+      <div className='inputLine'/>
+    </InputWrap>
+
   );
 };
 
