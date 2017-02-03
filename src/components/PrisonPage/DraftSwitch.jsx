16381deca097904f827178bf43923fe9db837f8c
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-02-03 16:09:01 +0300

    square draft switch

--- src/components/PrisonPage/DraftSwitch.jsx
+++ src/components/PrisonPage/DraftSwitch.jsx
@@ -2,65 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Label = styled.label`
+  background-color: #f3f3f3;
   position: relative;
-  border-radius: 20px;
+  padding: 5px;
   overflow: hidden;
   cursor: pointer;
   & input {
     display: none;
   }
+  & span {
+    position: relative;
+    display: inline-block;
+    width: 125px;
+    text-align: center;
+    line-height: 40px;
+    font-family: 'PT Sans';
+    font-size: 12px;
+    font-weight: bold;
+    text-transform: uppercase;
+    z-index: 1;
+  }
 `;
 
 const Switcher = styled.div`
-  width: 200%;
-  margin-left: 0;
-  transition: margin 0.3s ease-in 0s;
-  &:before,
-  &:after {
-    display: block;
-    float: left;
-    width: 50%;
-    padding: 0;
-    line-height: 44px;
-    font-size: 12px;
-    font-family: 'PT Sans';
-    font-weight: bold;
-    text-transform: uppercase;
-    box-sizing: border-box;
-  }
-  &:before {
-    content: "черновик";
-    padding-left: 10px;
-    background-color: #f3f3f3;
-    color: rgba(0, 0, 0, .3);
-  }
-  &:after {
-    content: "опубликован";
-    padding-right: 10px;
-    background-color: #000;
-    color: #fff;
-    text-align: right;
-  }
-  input:checked ~ & {
-    margin-left: -100%;
-  }
-`;
-
-const Circle = styled.div`
-  width: 32px;
-  margin: 6px;
-  background: #000;
   position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  border: 2px solid #999999;
-  border-radius: 20px;
-  transition: all 0.3s ease-in 0s;
-  input:checked ~ & {
-    right: 115px;
-    background: #fff;
-  }
+  width: calc(50% - 5px);
+  left: ${props => !props.checked ? '5px' : '50%'};
+  top: 5px;
+  bottom: 5px;
+  background-color: #fff;
+  transition: 0.3s ease-in 0s;
+  z-index: 0;
 `;
 
 const DraftSwitch = (props) => {
@@ -73,8 +45,9 @@ const DraftSwitch = (props) => {
         checked={ published }
         onChange={ onChange.bind(null, !published) }
       />
-      <Switcher/>
-      <Circle/>
+      <span>ЧЕРНОВИК</span>
+      <span>ОПУБЛИКОВАННО</span>
+      <Switcher checked={ published }/>
     </Label>
   );
 };
